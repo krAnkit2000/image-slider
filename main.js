b3eabcda9c525428f28a17208e89451c171b31b8
ankit <ankitkumar897955@gmail.com>
2023-11-02 19:12:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const wrapper = document.querySelector(".cards-wrapper");
+
+const dots = document.querySelectorAll(".dot");
+
+let activeDotNum = 0;
+
+dots.forEach((dot, idx) => {
+  dot.setAttribute("data-num", idx);
+
+  dot.addEventListener("click", (e) => {
+    let clickedDotNum = e.target.dataset.num;
+
+    if (clickedDotNum == activeDotNum) {
+      return;
+    } else {
+      let displayArea = wrapper.parentElement.clientWidth;
+
+      let pixels = -displayArea * clickedDotNum;
+      wrapper.style.transform = "translateX(" + pixels + "px)";
+
+      dots[activeDotNum].classList.remove("active");
+
+      dots[clickedDotNum].classList.add("active");
+
+      activeDotNum = clickedDotNum;
+    }
+  });
+});
+
+prev.forEach((prev, idx) => {
+  prev.setAttribute("data-num", idx);
+
+  prev.addEventListener("click", (e) => {
+    let clickedDotNum = e.target.dataset.num;
+
+    if (clickedDotNum == activeDotNum) {
+      return;
+    } else {
+      let displayArea = wrapper.parentElement.clientWidth;
+
+      let pixels = -displayArea * clickedDotNum;
+      wrapper.style.transform = "translateX(" + pixels + "px)";
+
+      prev[activeprevNum].classList.remove("active");
+
+      prev[clickedprevNum].classList.add("active");
+
+      activeDotNum = clickedDotNum;
+    }
+  });
+});
